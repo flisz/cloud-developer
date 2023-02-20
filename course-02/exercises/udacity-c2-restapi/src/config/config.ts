8aa516b94aa1dbb9341210862a9b26f7dadfab22
Francis Sziebert <franksziebert@gmail.com>
2023-02-20 14:36:51 -0500

    adds JWT_SECRET and POSTGRES_DATABASE to env

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -2,7 +2,7 @@ export const config = {
   "dev": {
     "username": process.env.POSTGRES_USERNAME,
     "password": process.env.POSTGRES_PASSWORD,
-    "database": "postgres",
+    "database": process.env.POSTGRES_DATABASE,
     "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
     "aws_region": process.env.AWS_REGION,
@@ -10,7 +10,7 @@ export const config = {
     "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "jwt": {
-    "secret": " "
+    "secret": process.env.JWT_SECRET
   },
   "prod": {
     "username": "",
